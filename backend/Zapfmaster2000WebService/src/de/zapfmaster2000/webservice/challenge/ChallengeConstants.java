75baab7887a38ee77b1b32d94850d25d5135e020
Thomas Kipar <thomas.kipar@gmail.com>
2012-11-10 18:51:27 +0100

    Moved deprecated backend projects into own folder.

--- backend/Zapfmaster2000WebService/src/de/zapfmaster2000/webservice/challenge/ChallengeConstants.java
+++
@@ -1,7 +0,0 @@
-package de.zapfmaster2000.webservice.challenge;
-
-public class ChallengeConstants {
-
-	public static final String CHALLENGE_TYPE_1V1 = "1v1";
-
-}
--- backend/de.beeromat.core/src/java/de/beeromat/internal/core/handler/DrawingsHandlerListener.java
+++
@@ -1,9 +0,0 @@
-package de.beeromat.internal.core.handler;
-
-import de.beeromat.core.model.db.User;
-
-public interface DrawingsHandlerListener {
-	
-	public void onDrawingFinished(User pUser);
-
-}
