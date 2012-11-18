c43e7e07e6cf00498ea9b022e4190107eda861dc
Ben Lenser <benlenser@ben-lensers-mac-mini.fritz.box>
2012-11-18 23:04:53 +0100

    Path to rest/news changed

--- backend/Zapfmaster2000RESTful/src/main/webapp/js/Modules/Constants.js
+++ backend/Zapfmaster2000RESTful/src/main/webapp/js/Modules/Constants.js
@@ -40,7 +40,7 @@ ZMO.modules.Constants = {
 		urls:{
 			FRONTPAGESTATS:"/tmp/stats_fp.json",
 			//NEWSLIST:"/tmp/news.json"
-			NEWSLIST:"http://192.168.178.38:8080/Zapfmaster2000RESTful/news"
+			NEWSLIST:"rest/news"
 		}
 
 }
