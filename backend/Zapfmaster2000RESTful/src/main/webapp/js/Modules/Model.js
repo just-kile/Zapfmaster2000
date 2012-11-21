9b3867ca3949a1ca8af9376c472e2ee310dfdcd5
Ben Lenser <benlenser@Ben-Lensers-Mac-mini.local>
2012-11-21 22:57:30 +0100

    Model changed for new JSON

--- backend/Zapfmaster2000RESTful/src/main/webapp/js/Modules/Model.js
+++ backend/Zapfmaster2000RESTful/src/main/webapp/js/Modules/Model.js
@@ -7,41 +7,44 @@ ZMO.DrawfeedModel = function(config){
 	this.timestamp = config.timestamp;
 	this.place = "Harzj";
 	this.keg = config.keg;
-}
+};
 ZMO.NewsModel =function(config){
-	this.name = config.name||config.user;
+	this.name = config.name||config.user||config.user_name;
 	this.amount = Math.round(config.amount*100)/100;
 	this.duration = config.duration;
-	this.date = config.date||(new Date()). toGMTString()
+	this.date = config.date||(new Date()). toGMTString();
 	this.place = config.place;
 	this.keg =config.keg;
 	this.brand = config.brand||config.BRAND;
-	this.image =config.image||config.IMAGE_PATH;
-	this.userid = config.userid;
-}
+	this.image =config.image||config.IMAGE_PATH||config.user_image;
+	
+	this.userid = config.userid||config.user_id;
+	this.type = config.type;
+};
 ZMO.AchievementModel = function(config){
-	this.name = config.name||config.ACHIEVEMENT_NAME;
-	this.userid = config.userid;
-	this.username = config.username|| config.user;
+	this.userid = config.userid||config.user_id;
+	this.username = config.username|| config.user || config.user_name;
+	this.userimage = config.user_image;
 	this.date = config.date||(new Date()). toGMTString();
 	this.type = config.type;
 	this.achievement_id = config.achievement_id||config.ACHIEVEMENT_ID;
+	this.name = config.name||config.ACHIEVEMENT_NAME||config.achievement_name;
 	this.description = config.description;
-	this.image =config.image||config.ACHIEVEMENT_IMAGE_PATH;
-	this.public = config.public;
+	this.image =config.image||config.ACHIEVEMENT_IMAGE_PATH||config.achievement_image;
+	this.is_public = config.is_public;
 	
-}
+};
 ZMO.OtherModel = function(config){
 	this.text = config.text;
 	this.image = config.image||config.IMAGE_PATH||config.imagepath;
 	this.type = config.type;
-	this.date = new Date();
+	this.date = config.date||(""+new Date());
 	
-}
+};
 ZMO.ChallengeStartedModel = function(config){
 	this.userid = config.userid;
 	this.username = config.username;
-	this.userimage = config.userimage
+	this.userimage = config.userimage;
 	this.start_time = config.start_time;
 	this.type = config.type;
 	this.challenge_type = config.challenge_type;
@@ -49,7 +52,7 @@ ZMO.ChallengeStartedModel = function(config){
 	this.team = config.team;
 	this.image =config.image;
 	this.duration = config.duration;
-}
+};
 ZMO.GlobalChallengeModel = function(config){
 	this.type = config.type;
 	this.challenge_type = config.challenge_type;
@@ -62,7 +65,7 @@ ZMO.GlobalChallengeModel = function(config){
 			
 		});
 		return arr;
-	}
+	};
 	var sumArr =function(arr){
 		var count = 0;
 		for (var i = 0; i < arr.length; i++ )
@@ -70,21 +73,21 @@ ZMO.GlobalChallengeModel = function(config){
 			count += arr[i];
 		}
 		return count;
-	}
-	this.team1 = parseTeam(config.team1,"username")
-	this.team2 = parseTeam(config.team2,"username")
-	this.team1Images = parseTeam(config.team1,"userimage")
-	this.team2Images = parseTeam(config.team2,"userimage")
+	};
+	this.team1 = parseTeam(config.team1,"username");
+	this.team2 = parseTeam(config.team2,"username");
+	this.team1Images = parseTeam(config.team1,"userimage");
+	this.team2Images = parseTeam(config.team2,"userimage");
 	this.team1Amount = sumArr(parseTeam(config.team1,"amount"));
 	this.team2Amount = sumArr(parseTeam(config.team2,"amount"));
 	this.image = config.image;
 	this.date = config.start_time;
 	this.challengeId = config.challenge_id;
-}
+};
 
 ZMO.ModuleModel  =function(id,element,ratio){
 	this.id= id;
 	this.element= element;
 	this.ratio= ratio;
-}
+};
 
