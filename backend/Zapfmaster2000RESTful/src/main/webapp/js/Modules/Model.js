f13edd0f71e8c6d5c86a72f0772e171055d13bc7
Thomas Kipar <kipar@informatik.hu-berlin.de>
2012-11-27 20:14:26 +0100

    Fixed problems on access on possibly undefined properties.

--- backend/Zapfmaster2000RESTful/src/main/webapp/js/Modules/Model.js
+++ backend/Zapfmaster2000RESTful/src/main/webapp/js/Modules/Model.js
@@ -99,11 +99,16 @@ ZMO.RfidModel = function(conf){
 ZMO.MemberModel = function(config) {
 	this.userName = config.userName;
 	this.userId = config.userId;
-	this.achievements = config.achievements;	
-	this.totalAmount = config.totalAmount.toFixed(2);
+	this.achievements = config.achievements;
+	if (config.totalAmount != undefined) {
+		this.totalAmount = config.totalAmount.toFixed(2);
+	} else {
+		config.totalAmount = 0;
+	}
 	
+	// ignore all achievements if there are way to many
 	var max = ZMO.modules.Constants.member.MAX_ACHIEVEMENTS;
-	if (this.achievements.length > max) {
+	if (this.achievements != undefined && this.achievements.length > max) {
 		this.additional = true;
 		this.additionalCount = config.achievements.length - max;
 		this.achievements.splice(max, this.achievements.length - max);
