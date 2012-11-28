5523260f825098c7d910f76c335b116905b6c47d
Thomas Kipar <kipar@informatik.hu-berlin.de>
2012-11-28 17:58:04 +0100

    Finished implementation of the AchievementsReource (including unit test)

--- backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/achievements/AchievementsResource.java
+++ backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/achievements/AchievementsResource.java
@@ -1,7 +1,9 @@
 package de.kile.zapfmaster2000.rest.api.achievements;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -15,6 +17,7 @@ import javax.ws.rs.core.Response.Status;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import de.kile.zapfmaster2000.rest.api.achievements.AchievementResonse.UserThatGained;
 import de.kile.zapfmaster2000.rest.core.Zapfmaster2000Core;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Account;
 
@@ -35,25 +38,45 @@ public class AchievementsResource {
 			Transaction tx = session.beginTransaction();
 
 			@SuppressWarnings("unchecked")
-			List<Object[]> result = session.createQuery(
-					"SELECT a.id, a.name, a.descripiton, a.imagePath FROM a")
-					.list();
+			List<Object[]> result = session
+					.createQuery(
+							"SELECT a.id, a.name, a.description, a.imagePath, "
+									+ "u.id, u.name, u.imagePath "
+									+ "FROM Achievement a "
+									+ "LEFT JOIN a.gained g LEFT JOIN g.user u "
+									+ "WITH u.account.id = :accountId "
+									+ "ORDER BY a.name ASC, g.date DESC")
+					.setLong("accountId", account.getId()).list();
 
 			tx.commit();
 
-			List<AchievementResonse> response = new ArrayList<>();
-			for (Object[] resultRow : result) {
-				AchievementResonse resp = new AchievementResonse();
-				resp.setAchivementId((Long) resultRow[0]);
-				resp.setAchievementName((String) resultRow[1]);
-				resp.setAchievementDescription((String) resultRow[2]);
-				resp.setAchievementImage((String) resultRow[3]);
+			Map<Long, AchievementResonse> mapIdToResp = new LinkedHashMap<>();
 
-				response.add(resp);
+			for (Object[] resultRow : result) {
+				long id = (Long) resultRow[0];
+				AchievementResonse resp;
+				if (mapIdToResp.containsKey(id)) {
+					resp = mapIdToResp.get(id);
+				} else {
+					resp = new AchievementResonse();
+					resp.setAchivementId((Long) resultRow[0]);
+					resp.setAchievementName((String) resultRow[1]);
+					resp.setAchievementDescription((String) resultRow[2]);
+					resp.setAchievementImage((String) resultRow[3]);
+					mapIdToResp.put(id, resp);
+				}
+
+				if (resultRow[4] != null) {
+					// a user gained that achievement
+					UserThatGained user = new UserThatGained();
+					user.setUserId((Long) resultRow[4]);
+					user.setUserName((String) resultRow[5]);
+					user.setUserImage((String) resultRow[6]);
+					resp.getUsers().add(user);
+				}
 			}
 
-			return Response.ok(response).build();
-
+			return Response.ok(new ArrayList<>(mapIdToResp.values())).build();
 		}
 
 		return Response.status(Status.FORBIDDEN).build();
