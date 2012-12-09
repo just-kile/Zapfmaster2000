1f7429d4c67c6faf5494f6f2bcc63cca917b3750
Thomas Kipar <kipar@informatik.hu-berlin.de>
2012-12-09 13:23:36 +0100

    Fixed unit tests.

--- backend/Zapfmaster2000RESTful/src/test/java/de/kile/zapfmaster2000/rest/core/news/TestNewsService.java
+++ backend/Zapfmaster2000RESTful/src/test/java/de/kile/zapfmaster2000/rest/core/news/TestNewsService.java
@@ -1,6 +1,7 @@
 package de.kile.zapfmaster2000.rest.core.news;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,6 +14,7 @@ import org.junit.Test;
 
 import de.kile.zapfmaster2000.rest.AbstractDatabaseTest;
 import de.kile.zapfmaster2000.rest.core.Zapfmaster2000Core;
+import de.kile.zapfmaster2000.rest.core.achievement.AchievementService;
 import de.kile.zapfmaster2000.rest.core.box.BoxService;
 import de.kile.zapfmaster2000.rest.core.box.BoxServiceListener;
 import de.kile.zapfmaster2000.rest.core.box.DrawService;
@@ -21,12 +23,14 @@ import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Account;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Box;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Drawing;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.DrawingNews;
+import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Keg;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.News;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.User;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Zapfmaster2000Factory;
 
 public class TestNewsService extends AbstractDatabaseTest {
 
+	private Keg keg;
 	private User user;
 	private Box box;
 	private Account account;
@@ -38,7 +42,10 @@ public class TestNewsService extends AbstractDatabaseTest {
 		user.setImagePath("img/user1");
 
 		box = Zapfmaster2000Factory.eINSTANCE.createBox();
-
+		
+		keg = Zapfmaster2000Factory.eINSTANCE.createKeg();
+		keg.setBox(box);
+		
 		account = Zapfmaster2000Factory.eINSTANCE.createAccount();
 		account.getBoxes().add(box);
 
@@ -46,11 +53,13 @@ public class TestNewsService extends AbstractDatabaseTest {
 		drawing.setDate(new Date());
 		drawing.setAmount(0.123);
 		drawing.setUser(user);
+		drawing.setKeg(keg);
 
 		Session session = Zapfmaster2000Core.INSTANCE.getTransactionService()
 				.getSessionFactory().getCurrentSession();
 		Transaction tx = session.beginTransaction();
 
+		session.save(keg);
 		session.save(user);
 		session.save(box);
 		session.save(account);
@@ -62,7 +71,7 @@ public class TestNewsService extends AbstractDatabaseTest {
 	@Test
 	public void simpleEndDraw() {
 		BoxServiceMock boxServiceMock = new BoxServiceMock();
-		new NewsServiceImpl(boxServiceMock);
+		new NewsServiceImpl(boxServiceMock, mock(AchievementService.class));
 
 		for (BoxServiceListener listener : boxServiceMock.listeners) {
 			listener.onEndDrawing(box, drawing);
