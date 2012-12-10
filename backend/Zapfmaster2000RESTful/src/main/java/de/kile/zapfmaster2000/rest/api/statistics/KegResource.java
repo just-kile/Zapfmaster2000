a71f4f6693cd5cc03abbe0f7d9ebba47b743e0a9
PapaBravo <paul.boeck@gmx.de>
2012-12-11 00:12:23 +0100

    added KegResource and according unit test

---
+++ backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/statistics/KegResource.java
@@ -0,0 +1,69 @@
+package de.kile.zapfmaster2000.rest.api.statistics;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import de.kile.zapfmaster2000.rest.core.Zapfmaster2000Core;
+import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Account;
+
+public class KegResource {
+
+	private static final Logger LOG = Logger.getLogger(RankingsResource.class);
+
+	@Path("kegStats")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response retrieveKegStats(@QueryParam("token") String pToken) {
+		Account account = Zapfmaster2000Core.INSTANCE.getAuthService()
+				.retrieveAccount(pToken);
+		if (account != null) {
+			Session session = Zapfmaster2000Core.INSTANCE
+					.getTransactionService().getSessionFactory()
+					.getCurrentSession();
+			Transaction tx = session.beginTransaction();
+
+			// TODO restrict to Box?
+
+			@SuppressWarnings("unchecked")
+			List<Object> resultCurrentKeg = session
+					.createQuery(
+							"SELECT k.id, k.brand, k.size, k.startDate "
+									+ " FROM Keg k, Drawing d "
+									+ " WHERE d.keg = k "
+									+ " ORDER BY d.date DESC").setMaxResults(1)
+					.list();
+
+			@SuppressWarnings("unchecked")
+			List<Object> resultNumberKegs = session.createQuery(
+					"SELECT COUNT (DISTINCT k.id) FROM Keg k, Drawing d"
+							+ " WHERE d.keg = k").list();
+
+			tx.commit();
+
+			KegResponse response = new KegResponse();
+
+			Object[] resultRow = (Object[]) resultCurrentKeg.get(0);
+			response.setKegId((Long) resultRow[0]);
+			response.setBrand((String) resultRow[1]);
+			response.setSize((Integer) resultRow[2]);
+			response.setStart_date((Date) resultRow[3]);
+			response.setKegNumbers((Long) resultNumberKegs.get(0));
+
+			return Response.ok(response).build();
+		}
+
+		return Response.status(Status.FORBIDDEN).build();
+	}
+}
---
+++ backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/statistics/KegResponse.java
@@ -0,0 +1,60 @@
+package de.kile.zapfmaster2000.rest.api.statistics;
+
+import java.util.Date;
+
+public class KegResponse {
+
+	/**
+	 * Id of the current keg.
+	 */
+	private long kegId;
+	private String brand;
+	private int size;
+	private Date start_date;
+
+	/**
+	 * Number of kegs drunk so far (including the current one)
+	 */
+	private long kegNumbers;
+
+	public long getKegId() {
+		return kegId;
+	}
+
+	public void setKegId(long kegId) {
+		this.kegId = kegId;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public Date getStartDate() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public long getKegNumber() {
+		return kegNumbers;
+	}
+
+	public void setKegNumbers(long kegNumbers) {
+		this.kegNumbers = kegNumbers;
+	}
+
+}
