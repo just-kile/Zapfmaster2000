835c21847c87d28d2d6175c214260209be591f4d
Ben Lenser <benlenser@ben-lensers-mac-mini.fritz.box>
2012-12-12 22:09:26 +0100

    Moved PositionModel to Modules/Model.js

--- backend/Zapfmaster2000RESTful/src/main/webapp/js/App/Main.js
+++ backend/Zapfmaster2000RESTful/src/main/webapp/js/App/Main.js
@@ -1,3 +1,10 @@
+
+/**
+* Main Initializer for App
+*
+* @class MyClass
+* @constructor
+*/
 var ZMO = ZMO || {};
 /**
  * Loads all js and starts, when finished
@@ -5,15 +12,36 @@ var ZMO = ZMO || {};
 (function(){
 	head.js(
 			"js/App/Model/Constants.js",
+			//Util
 			"js/Modules/Util/Util.js",
 			"js/Modules/Util/Browser/Throbber.js",
 			"js/Modules/Util/Net/Ajax.js",
 			"js/Modules/Util/Object/Exists.js",
 			"js/Modules/Util/Globalfunctions.js",
+			//Modules
+			"js/Modules/Model.js" ,
+			"js/Modules/Stats/StatsModels.js",
+			"js/Modules/Constants.js",
+			"js/Modules/Drawfeed/Drawfeed.js",
+			"js/Modules/Navigation/Navigation.js",
+			"js/Modules/Stats/Kegstatus/KegstatusView.js",
+			"js/Modules/Stats/Kegstatus/Kegstatus.js",
+			"js/Modules/Stats/LineChart/LineChartView.js",
+			"js/Modules/Stats/LineChart/LineChart.js" ,
+			"js/Modules/Stats/Bestlist/BestlistView.js",
+			"js/Modules/Stats/Bestlist/Bestlist.js",
+			"js/Modules/Stats/AchievementBestlist/AchievementBestlist.js",
+			"js/Modules/Challenges/Challenges.js",
+			"js/Modules/Frontpagestats/FrontpageStats.js",
+			"js/Modules/Members/Members.js",
+			"js/Modules/Achievements/Achievements.js",
+			"js/Modules/Achievementstats/Achievementstats.js",
+			"js/Modules/Achievementlist/Achievementlist.js",
+
 			//main
-			
 			"js/App/Model/Model.js",
 			"js/App/View/View.js",
+			"js/App/modules_properties.js",
 			"js/App/Controller/Controller.js",
 			function() {
 				if(ZMO.controller)ZMO.controller.init();
