f56f42673f4e99756da4342f722fcc6683058147
PapaBravo <paul.boeck@gmx.de>
2012-12-13 14:45:30 +0100

    added AchievementResource and according unit test

--- backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/statistics/AchievementResource.java
+++ backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/statistics/AchievementResource.java
@@ -16,13 +16,15 @@ import org.hibernate.Transaction;
 import de.kile.zapfmaster2000.rest.core.Zapfmaster2000Core;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Account;
 
-public class AmountResource {
+public class AchievementResource {
+
+	// TODO add specific to user
 
 	@SuppressWarnings("unchecked")
-	@Path("amountStats")
+	@Path("achievementsStats")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response retrieveCurrentKegAmount(@QueryParam("token") String pToken) {
+	public Response retrieveAchievementStats(@QueryParam("token") String pToken) {
 		Account account = Zapfmaster2000Core.INSTANCE.getAuthService()
 				.retrieveAccount(pToken);
 
@@ -33,33 +35,22 @@ public class AmountResource {
 			Transaction tx = session.beginTransaction();
 
 			// TODO restrict to box
-
-			List<Object> resultAmountCurrentKeg = session
-					.createQuery(
-							"SELECT SUM(d.amount)" + " FROM Keg k, Drawing d"
-									+ " WHERE d.keg = k GROUP BY k.id "
-									+ " ORDER BY MAX(d.date) DESC")
-					.setMaxResults(1).list();
-
-			// TODO restrict to box
-			List<Object[]> resultAmounts = session.createQuery(
-					"SELECT SUM(d.amount), MAX(d.amount) FROM Drawing d")
-					.list();
+			List<Object> resultAchievementCount = session.createQuery(
+					"SELECT COUNT(g.id) FROM GainedAchievement g").list();
 
 			List<Object> resultMostActivity = session
 					.createQuery(
-							"SELECT HOUR(d.date) FROM Drawing d"
-									+ " GROUP BY HOUR(d.date)"
-									+ " ORDER BY SUM(d.amount) DESC")
+							"SELECT HOUR(g.date) FROM GainedAchievement g"
+									+ " GROUP BY HOUR(g.date)"
+									+ " ORDER BY COUNT(g.id) DESC")
 					.setMaxResults(1).list();
 
 			tx.commit();
 
-			AmountResponse response = new AmountResponse();
-			response.setAmountCurrentKeg((Double) resultAmountCurrentKeg.get(0));
-			response.setAmountTotal((Double) resultAmounts.get(0)[0]);
-			response.setGreatestDrawing((Double) resultAmounts.get(0)[1]);
-			response.setMostActivityHour((Integer) resultMostActivity.get(0));
+			AchievementResponse response = new AchievementResponse();
+
+			response.setCount((Long) resultAchievementCount.get(0));
+			response.setMostAchievementHour((Integer) resultMostActivity.get(0));
 
 			return Response.ok(response).build();
 		}
