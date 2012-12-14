d9cf6a4213451d07b13733935e23d7e387fcd2e7
Ben Lenser <benlenser@Ben-Lensers-Mac-mini.local>
2012-12-14 14:13:23 +0100

    Changed link paths from absolute to relative

--- backend/Zapfmaster2000RESTful/src/main/webapp/js/Core/Controller/Controller.js
+++ backend/Zapfmaster2000RESTful/src/main/webapp/js/Core/Controller/Controller.js
@@ -6,24 +6,23 @@ ZMO.controller = (function($,document,view,ajax){
  * Gets called when a page changes
  */
 	var onPageChange =function(event,datas){
-		
-				var hash = window.location.hash;
-				ajax.resetQueue();
-				ajax.stopPull();
-				ajax.abortPushRequests();
-				$(window).unbind("scroll");
-				//var hash = Object.keys(url)[0];
-				var arr=[],datas={};
-				if(hash){
-					arr = ZMO.hashHandler.queryToObj(hash);
-					url = arr[0];
-					datas = arr[1];
-				}
-				if(typeof hash=="undefined" || hash=="" || typeof mP[url] =="undefined"){
-					view.createPage("front",mP.front);
-				}else{
-					view.createPage(url,mP[url],datas);
-				}
+		var hash = window.location.hash;
+		ajax.resetQueue();
+		ajax.stopPull();
+		ajax.abortPushRequests();
+		$(window).unbind("scroll");
+		//var hash = Object.keys(url)[0];
+		var arr=[],datas={};
+		if(hash){
+			arr = ZMO.hashHandler.queryToObj(hash);
+			url = arr[0];
+			datas = arr[1];
+		}
+		if(typeof hash=="undefined" || hash=="" || typeof mP[url] =="undefined"){
+			view.createPage("front",mP.front);
+		}else{
+			view.createPage(url,mP[url],datas);
+		}
 		
 	};
 	var checkLoginStatus =function(successCallb,errorCallb){
