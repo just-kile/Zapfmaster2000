6cea5bbdc314e58f7285d145107fd8bbe8555502
PapaBravo <paul.boeck@gmx.de>
2012-12-23 17:55:42 +0100

    added RankResource and according builder and unit tests.

--- backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/statistics/RankResource.java
+++ backend/Zapfmaster2000RESTful/src/main/java/de/kile/zapfmaster2000/rest/api/statistics/RankResource.java
@@ -16,37 +16,33 @@ import de.kile.zapfmaster2000.rest.model.zapfmaster2000.Account;
 import de.kile.zapfmaster2000.rest.model.zapfmaster2000.User;
 
 @Path("statistics")
-public class AlcoholLevelResource {
+public class RankResource {
 
 	private static final Logger LOG = Logger.getLogger(RankingsResource.class);
 
-	// TODO add test
+	// TODO add user independent
+
 	/**
-	 * Returns {@link AlcoholLevelResponse} for an user with id
-	 * <code>pUser</code>.
+	 * Returns {@link RankResponse} for an user with id <code>pUser</code>.
 	 * 
 	 * @param pToken
 	 *            Token of {@link Account} used for authentication.
 	 * @param pUser
 	 *            Valid {@link User} id.
 	 * @return <ul>
-	 *         <li>OK: {@link AlcoholLevelResponse}</li>
+	 *         <li>OK: {@link RankResponse}</li>
 	 *         <li>BAD_REQUEST: if <code>pUser</code> cannot be parsed.</li>
 	 *         <li>FORBIDDEN: if authentication with <code>pToken</code> fails.</li>
 	 *         </ul>
 	 */
-	@Path("alcoholLevel")
+	@Path("rank")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response retrieveAlcoholLevel(@QueryParam("token") String pToken,
+	public Response retrieveRank(@QueryParam("token") String pToken,
 			@QueryParam("user") String pUser) {
 		Account account = Zapfmaster2000Core.INSTANCE.getAuthService()
 				.retrieveAccount(pToken);
 
-		if (pUser == null) {
-			LOG.error("The parameter pUser is mandatory.");
-			return Response.status(Status.BAD_REQUEST).build();
-		}
 		long user = -1;
 
 		try {
