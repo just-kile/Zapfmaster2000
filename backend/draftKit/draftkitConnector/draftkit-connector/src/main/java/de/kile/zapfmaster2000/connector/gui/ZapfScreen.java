a251c7d2f02cbfc7368102e7313b6f5fc9c55662
wittekind <dawittekind@gmail.com>
2012-12-29 22:03:03 +0100

    Initial commit ZapfScreen

---
+++ backend/draftKit/draftkitConnector/draftkit-connector/src/main/java/de/kile/zapfmaster2000/connector/gui/ZapfScreen.java
@@ -0,0 +1,42 @@
+package de.kile.zapfmaster2000.connector.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ZapfScreen extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ZapfScreen frame = new ZapfScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ZapfScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
