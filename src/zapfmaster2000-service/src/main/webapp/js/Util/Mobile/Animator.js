440a994de15d94da2155e59dd52016c773f03e8a
Ben Lenser <lenser.ben@gmail.com>
2013-05-28 18:16:47 +0200

    Added android Gingerbread support

--- src/zapfmaster2000-service/src/main/webapp/js/Util/Mobile/Animator.js
+++ src/zapfmaster2000-service/src/main/webapp/js/Util/Mobile/Animator.js
@@ -16,14 +16,27 @@ ZMO.Utils.animator = (function($,document){
 	var setContainer = function(container){
 		return sCont = container;
 	};
+	
+	var isWebkitAvailable = (function(){
+		var enable = true;
+		//Disable animation for android < sdk version 13
+		if(typeof device!="undefined" && device.platform=="Android" &&  parseInt(device.version.split(".").join(""))<300 ){
+			enable = false;
+		}
+		return typeof document.body.style.webkitTransition!="undefined" && enable;
+	})();
 	var prepareContainer = function(sideContainer,right){
-		sideContainer.addClass(!right?"animate":"animateRight");
-		$(sideContainer)[0].addEventListener( 'webkitTransitionEnd', 
-			    function( event ) {
-
-			if(getCallback())getCallback()();
-			}, false );
 		
+		if(isWebkitAvailable){
+			sideContainer.addClass(!right?"animate":"animateRight");
+			$(sideContainer)[0].addEventListener( 'webkitTransitionEnd', 
+				    function( event ) {
+	
+				if(getCallback())getCallback()();
+				}, false );
+		}else{
+			sideContainer.addClass(!right?"animate-noWk":"animateRight-noWk");
+		}
 	};
 	/**
 	 * Animates the left open navigation
@@ -34,13 +47,25 @@ ZMO.Utils.animator = (function($,document){
 
 		sideContainer.show();
 		
-		sideContainer.addClass("animateSide");
+		
 		openPanel = true;
+		if(isWebkitAvailable){
+			sideContainer.addClass("animateSide");
+		}else{
+			if(getCallback())getCallback()();
+			sideContainer.addClass("animateSide-noWk");
+		}
 	};
 	var animateLeftRev = function(mainContainer,sideContainer,callback){
 		setCallback(callback);
-		sideContainer.removeClass("animateSide animateChall");
+		
 		openPanel = false;
+		if(isWebkitAvailable){
+			sideContainer.removeClass("animateSide animateChall");
+		}else{
+			if(getCallback())getCallback()();
+			sideContainer.removeClass("animateSide-noWk animateChall-noWk");
+		}
 	};
 
 	var animateRight = function(mainContainer,sideContainer,callback){
@@ -49,15 +74,28 @@ ZMO.Utils.animator = (function($,document){
 
 		sideContainer.addClass("animateChall");
 		openPanel = true;
+		if(isWebkitAvailable){
+			sideContainer.addClass("animateChall");
+		}else{
+			if(getCallback())getCallback()();
+			sideContainer.addClass("animateChall-noWk");
+		}
 	};
 	var animateRightRev = function(mainContainer,sideContainer,callback){
 		setCallback(callback);
 		sideContainer.removeClass("animateSide animateChall");
 		openPanel = false;
+		if(isWebkitAvailable){
+			sideContainer.removeClass("animateSide animateChall");
+		}else{
+			if(getCallback())getCallback()();
+			sideContainer.removeClass("animateSide-noWk animateChall-noWk");
+		}
 	};
 	var init = function(container,right){
 		prepareContainer(container,right);
 	}
+	
 	var isPanelOpen = function(){
 		return openPanel;
 	}
