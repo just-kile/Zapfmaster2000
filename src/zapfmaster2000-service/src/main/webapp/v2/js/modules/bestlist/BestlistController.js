09b2439ca68ec937d850519accfe58322f96e131
Ben Lenser <lenser.ben@gmail.com>
2013-11-29 17:11:10 +0100

    Bestlist: Added amount and reorder

--- src/zapfmaster2000-service/src/main/webapp/v2/js/modules/bestlist/BestlistController.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/modules/bestlist/BestlistController.js
@@ -5,11 +5,11 @@ define(['Console', 'Underscore'], function (Console, _) {
     var controller = ['$scope', 'CometService', 'DataService', "ZMConstants", function ($scope, CometService, ajax, c) {
         Console.group("Bestlist controller entered.");
         $scope.baseUrl = c.baseUrl;
-        var calcPercentAmountAndOrder = function (data) {
+        var calcPercentAmount = function (data) {
             var sum = 0, sumAdd = 0, amount = 0, len = data.length;
             _.each(data, function (user, index) {
                 sum += user.amount;
-                user.order = index;
+
                 user.amountPercent = 0;
             });
 
@@ -21,18 +21,57 @@ define(['Console', 'Underscore'], function (Console, _) {
             });
             return data;
         }
+        var initOrder = function(data){
+            _.each(data,function(user,index){
+                user.order = index;
+            });
+            return data;
+        }
         var initScope = function () {
             ajax.getDatas(c.bestlistUrl, function (data) {
                 if (data.length > c.bestlistMax) {
                     data.length = c.bestlistMax;
                 }
-                calcPercentAmountAndOrder(data);
+                initOrder(data);
+                calcPercentAmount(data);
                 $scope.bestlist = data;
             });
         };
+        var sortDatas = function(){
+            var len = $scope.bestlist.length,tmp=0;
+            for(var i=0;i<len;i++){
+                for(var j=0;j<len;j++){
+                    if($scope.bestlist[i].amount >$scope.bestlist[j].amount){
+                         tmp = $scope.bestlist[i].order;
+                        $scope.bestlist[j].order=j;
+                        $scope.bestlist[i].order=tmp;
+                    }
+                }
+            }
+        }
+        var updateScope = function(data){
+            ajax.getDatas(c.bestlistUrl, function (data) {
+                if (data.length > c.bestlistMax) {
+                    data.length = c.bestlistMax;
+                }
+                calcPercentAmount(data);
+                var bestlist = $scope.bestlist;
 
+                _.each(data,function(user,index){
+                    var userId = user.id;
+                    _.each(bestlist,function(oldUser,oldIndex){
+                         if(oldUser.id == userId){
+                             $scope.bestlist[oldIndex].order = index;
+                             $scope.bestlist[oldIndex].amount = user.amount;
+                             $scope.bestlist[oldIndex].amountPercent = user.amountPercent;
+                         }
+                    });
+                });
+
+            });
+        }
         CometService.addPushListener(function (data) {
-
+           updateScope();
         });
         initScope();
         Console.groupEnd();
