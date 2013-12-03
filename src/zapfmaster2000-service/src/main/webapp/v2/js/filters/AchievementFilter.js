d63170cd3cf242162d9b946733bde1d8f2c08637
Ben Lenser <lenser.ben@gmail.com>
2013-12-03 21:29:22 +0100

    Added missing files

---
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/filters/AchievementFilter.js
@@ -0,0 +1,14 @@
+define(['Console'], function (Console) {
+    "use strict";
+    Console.group("Entering AchievementFilter module.");
+
+    var service = ['ZMConstants', function (c) {
+        return function(input) {
+                return ""+input
+        };
+
+    }];
+
+    Console.groupEnd();
+    return service;
+});
