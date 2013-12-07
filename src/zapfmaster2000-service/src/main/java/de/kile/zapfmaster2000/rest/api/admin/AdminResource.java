2c15a1f84815e624790a8223332620652c1f61e3
thomaskipar <thomas.kipar@gmail.com>
2013-12-07 17:10:03 +0100

    #157: Added missing media type.

--- src/zapfmaster2000-service/src/main/java/de/kile/zapfmaster2000/rest/api/admin/AdminResource.java
+++ src/zapfmaster2000-service/src/main/java/de/kile/zapfmaster2000/rest/api/admin/AdminResource.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -88,6 +90,7 @@ public class AdminResource {
 
 	@GET
 	@Path("/account")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response retrieveAccountAdmins(@QueryParam("token") String token) {
 		Admin admin = Zapfmaster2000Core.INSTANCE.getAuthService()
 				.retrieveAdmin(token);
