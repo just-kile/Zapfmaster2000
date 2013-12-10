d3b603522ae0acbd370544670d7132fa392b7190
Ben Lenser <lenser.ben@gmail.com>
2013-12-10 17:34:35 +0100

    Added Achievement images to userstats

--- src/zapfmaster2000-service/src/main/webapp/v2/js/widgets/newsstack/NewsstackController.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/widgets/newsstack/NewsstackController.js
@@ -2,10 +2,28 @@ define(['Console'], function (Console) {
     "use strict";
     Console.group("Entering Newsstack controller module.");
 
-    var controller = ['$scope',function ($scope) {
+    var controller = ['$scope', '$timeout', 'CometService', 'DataService', "ZMConstants",
+        function ($scope, $timeout, CometService, ajax, c) {
         Console.group("Newsstack controller entered.");
+            CometService.addPushListener(function (data) {
+                if (c.DRAWING == data.type){
+                    addToNewsQueue(data);
+                }
+            });
+            var addToNewsQueue = function(data){
+                $scope.news.push(data);
+                /*$timeout(function(){
+                   $scope.shift();
+                },5000);   */
+            }
+            var initScope = function(){
+                $scope.news = [];
 
-        Console.groupEnd();
+            }
+                initScope();
+
+
+            Console.groupEnd();
     }];
     //controller.$inject = [];
 
