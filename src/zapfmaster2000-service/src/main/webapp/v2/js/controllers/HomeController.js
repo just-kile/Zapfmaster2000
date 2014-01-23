fac1aa95e990e121e0db21a2c4e1b931fe9b7a29
Ben Lenser <lenser.ben@gmail.com>
2014-01-23 15:32:49 +0100

    Implemented widgetchange

--- src/zapfmaster2000-service/src/main/webapp/v2/js/controllers/HomeController.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/controllers/HomeController.js
@@ -6,6 +6,7 @@ define(['Console'], function (Console) {
         Console.group("HomeController entered.");
         $scope.widgetBaseUrl = c.widgetBaseUrl;
         var widgetTimeouts = {};
+        var firstWidgets = 0, widgetChangeEnabled=true;
         var rows = {
             topLeft: [
                 {
@@ -29,7 +30,7 @@ define(['Console'], function (Console) {
                     {
                         name: "challenges",
                         className: "col-md-4",
-                        interval: 10000
+                        interval: 7000
                     }
                 ],
                 [
@@ -41,7 +42,7 @@ define(['Console'], function (Console) {
                     {
                         name: "bestlist",
                         className: "col-md-4",
-                        interval: 7000
+                        interval: 12000
                     }
                 ]
             ]
@@ -50,20 +51,20 @@ define(['Console'], function (Console) {
             $scope.rows = {};
             $scope.rows.topLeft = rows.topLeft;
             $scope.rows.topRight = rows.topRight;
-            $scope.rows.center = rows.center[0];
+            $scope.rows.center = _.clone(rows.center[firstWidgets]);
             Console.debug("Initializing:", rows);
         };
 
-        var updateWidget = function (widget, index, actualRow) {
+        var updateWidget = function (widget, index,rows, actualRow) {
             actualRow = (actualRow+1) % rows.center.length;
             var newWidget = rows.center[actualRow] && rows.center[actualRow][index];
             $scope.rows.center.splice(index,1,newWidget);
-            (function(newWidget, index, actualRow){
+            (function(newWidget, index,rows, actualRow){
                 $timeout(function () {
-                    updateWidget(newWidget, index, actualRow);
+                    updateWidget(newWidget, index,rows ,actualRow);
                 }, newWidget.interval);
 
-            })(newWidget,index,actualRow);
+            })(newWidget,index,rows,actualRow);
 
         };
         var startUpdater = function (rows) {
@@ -75,7 +76,7 @@ define(['Console'], function (Console) {
             });
         }
 
-        //startUpdater(rows);
+       if(widgetChangeEnabled)startUpdater(rows);
 
 
         //controller.$inject = [];
