8d1e2b77f0f564bac306e416eece86f21073a0cf
Ben Lenser <lenser.ben@gmail.com>
2014-03-24 21:41:39 +0100

    #171: Implemented animation, added templates for all states

--- src/zapfmaster2000-service/src/main/webapp/v2/js/filters/MaxCountFilter.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/filters/MaxCountFilter.js
@@ -4,10 +4,14 @@ define(['Console',"Underscore"], function (Console,_) {
 
     var service = ['ZMConstants', function (c) {
         return function(input) {
+            if(input.length>2){
+                input.length=c.newsFeedLength;
+            }
 
-           return _.filter(input,function(val,index){
+              return input;
+          /* return _.filter(input,function(val,index){
                return index<2;
-           });
+           });   */
 
         };
 
--- src/zapfmaster2000-service/src/main/webapp/v2/js/widgets/newsstack/NewsstackController.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/widgets/newsstack/NewsstackController.js
@@ -4,44 +4,65 @@ define(['Console'], function (Console) {
 
     var controller = ['$scope', '$timeout', 'CometService', 'DataService', "ZMConstants",
         function ($scope, $timeout, CometService, ajax, c) {
-        Console.group("Newsstack controller entered.");
+            Console.group("Newsstack controller entered.");
             $scope.baseUrl = c.baseUrl;
-
+            var waitingQueue = [];
             CometService.addPushListener(function (data) {
-              addToNewsQueue(data);
+                addToNewsQueue(data);
             });
+            var queueTimeout = false;
+            var isRunning = false;
+            var startUpdating = function (force) {
+                if (waitingQueue.length > 0) {
+                    if(isRunning&&!force)return;
+                    $scope.news.unshift(waitingQueue.pop());
+                    queueTimeout = $timeout(function(){startUpdating(true)}, c.newsFeedTimeout);
+                    isRunning = true;
+                } else if(waitingQueue.length==0){
+                     isRunning = false;
+                }
 
-            var addToNewsQueue = function(data){
-                $scope.news.push(data);
-                $timeout(function(){
-                   $scope.news.shift();
-                },5000);
             }
-            var initScope = function(){
+            var addToNewsQueue = function (data) {
+                waitingQueue.push(data);
+                startUpdating();
+
+            }
+
+            var initScope = function () {
                 $scope.news = [];
 
             }
             initScope();
-            /*Test*/
-            var DUMMY_DATAS = {"type":"DRAWING","image":"rest/image/user/1","date":"20140323-184633","userId":1,"userName":"Ben","amount":0.12798644841493814,"kegId":1,"brand":"Carlsberg","location":"Kölle"};
-            addToNewsQueue(DUMMY_DATAS);
+            /*Testing, delete if not necessary*/
+            //addToNewsQueue(DUMMY_DATAS);
+            /*  var stopFlag = false;
+             var dummydata = function(){
 
-            $timeout(function(){
+             var DUMMY_DATAS = {"type":"DRAWING","image":"rest/image/user/1","date":"20140323-184633","userId":1,"userName":"Ben","amount":Math.random()*10,"kegId":1,"brand":"Carlsberg","location":"Kölle"};
 
+             addToNewsQueue(DUMMY_DATAS);
 
-                DUMMY_DATAS = {"type":"DRAWING","image":"rest/image/user/2","date":"20140323-184633","userId":2,"userName":"Jenny","amount":0.12798644841493814,"kegId":1,"brand":"Carlsberg","location":"Kölle"};
-                addToNewsQueue(DUMMY_DATAS);
-            },1000);
+             if(!stopFlag)$timeout(dummydata,4000);
+             //stopFlag = true;
+             }
 
+             dummydata();
+             stopFlag = false;   */
+            ajax.getDatas(c.newsFeedUrl, function (json) {
+                if(json){
+                    _.each(json.reverse(), function (data) {
+                        addToNewsQueue(data);
+                    });
+                }
 
-            $timeout(function(){
-
-                DUMMY_DATAS = {"type":"DRAWING","image":"rest/image/user/2","date":"20140323-184633","userId":2,"userName":"Peter","amount":0.12798644841493814,"kegId":1,"brand":"Carlsberg","location":"Kölle"};
-                addToNewsQueue(DUMMY_DATAS);
-            },2000);
+            }, {
+                start: 0,
+                length: c.newsFeedLength
+            })
 
             Console.groupEnd();
-    }];
+        }];
     //controller.$inject = [];
 
     Console.groupEnd();
