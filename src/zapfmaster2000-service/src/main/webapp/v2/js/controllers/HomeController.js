dfba93ee687baf0f0ff3308b18d0bc7719783975
Ben Lenser <lenser.ben@gmail.com>
2014-04-10 18:04:17 +0200

    #177 Implemented switch between half and third widgets

--- src/zapfmaster2000-service/src/main/webapp/v2/js/controllers/HomeController.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/controllers/HomeController.js
@@ -2,12 +2,19 @@ define(['Console'], function (Console) {
     "use strict";
     Console.group("Entering HomeController module.");
 
-    var controller = ['$scope','$routeParams', '$timeout', 'ZMConstants','$animate', function ($scope,$routeParams, $timeout, c,$animate) {
+    var controller = ['$scope', '$routeParams', '$timeout', 'ZMConstants', '$animate', function ($scope, $routeParams, $timeout, c, $animate) {
         Console.group("HomeController entered.");
         $scope.widgetBaseUrl = c.widgetBaseUrl;
         var widgetTimeouts = {};
-        var firstWidgets = $routeParams.widgetId || 0,
-            widgetChangeEnabled=typeof $routeParams.widgetId =="undefined";
+        var firstWidget = $routeParams.widgetId || 2,
+            widgetChangeEnabled = typeof $routeParams.widgetId == "undefined";
+
+        var rowsDivider = 2;//rows.centerHalf.length;
+        var activeSubWidget = firstWidget>=rowsDivider?"centerHalf":"centerThird";
+        firstWidget = firstWidget % rowsDivider;
+        function getActiveSubWidget(){
+            return activeSubWidget;
+        }
         var rows = {
             topLeft: [
                 {
@@ -21,62 +28,56 @@ define(['Console'], function (Console) {
                     className: "col-md-6"
                 }
             ],
-            center: [
+            centerThird: [
                 [
                     {
                         name: "bestlist",
                         className: "col-md-8",
-                        interval: 50000
+                        interval: 5000
                     },
                     {
                         name: "challenges",
                         className: "col-md-4",
                         interval: 5000
                     }
-                ],[
-                    {
-                        name: "bestlist",
-                        className: "col-md-8",
-                        interval: 50000
-                    },
-                    {
-                        name: "zapfmastersplash",
-                        className: "col-md-4",
-                        interval: 10000
-                    }
-                ],[
-                    {
-                        name: "bestlist",
-                        className: "col-md-8",
-                        interval: 50000
-                    },
+                ],
+                [
                     {
                         name: "newsfeed",
-                        className: "col-md-4",
-                        interval: 50000
-                    }
-                ],[
-                    {
-                        name: "bestlist",
                         className: "col-md-8",
-                        interval: 50000
+                        interval: 5000
                     },
                     {
                         name: "achievementfeed",
                         className: "col-md-4",
-                        interval: 50000
+                        interval: 5000
                     }
-                ],
+
+                ]
+            ],
+            centerHalf: [
                 [
                     {
-                        name: "bestlist",
+                        name: "linechart",
                         className: "col-md-6",
-                        interval: 50000
+                        interval: 5000
                     },
                     {
                         name: "amountchart",
                         className: "col-md-6",
-                        interval: 50000
+                        interval: 5000
+                    }
+                ],
+                [
+                    {
+                        name: "achievementfeed",
+                        className: "col-md-6",
+                        interval: 5000
+                    },
+                    {
+                        name: "newsfeed",
+                        className: "col-md-6",
+                        interval: 5000
                     }
                 ]
             ]
@@ -85,38 +86,42 @@ define(['Console'], function (Console) {
             $scope.rows = {};
             $scope.rows.topLeft = rows.topLeft;
             $scope.rows.topRight = rows.topRight;
-            $scope.rows.center = _.clone(rows.center[firstWidgets]);
+
+            $scope.rows.center = _.clone(rows[getActiveSubWidget()][firstWidget]);
             Console.debug("Initializing:", rows);
         };
+        var changeInterval = {};
 
-        var updateWidget = function (widget, index,rows, actualRow) {
-            actualRow = (actualRow+1) % rows.center.length;
-            var newWidget = rows.center[actualRow] && rows.center[actualRow][index];
-            $scope.rows.center.splice(index,1,newWidget);
-            (function(newWidget, index,rows, actualRow){
-                $timeout(function () {
-                    updateWidget(newWidget, index,rows ,actualRow);
+        var updateWidget = function (widget, index, rows, actualRow) {
+            actualRow = (actualRow + 1) % rows[getActiveSubWidget()].length;
+            var newWidget = rows[getActiveSubWidget()][actualRow] && rows[getActiveSubWidget()][actualRow][index];
+            $scope.rows.center.splice(index, 1, newWidget);
+            (function (newWidget, index, rows, actualRow) {
+                changeInterval[getActiveSubWidget()+index] =  $timeout(function () {
+                    updateWidget(newWidget, index, rows, actualRow);
                 }, newWidget.interval);
 
-            })(newWidget,index,rows,actualRow);
+            })(newWidget, index, rows, actualRow);
 
         };
-        var changeInterval;
+
         var startUpdater = function (rows) {
-            var startCenterWidgets = rows.center[0];
+            var startCenterWidgets = rows[getActiveSubWidget()][0];
             _.each(startCenterWidgets, function (widget, index) {
-                changeInterval =   $timeout(function () {
-                    updateWidget(widget, index, rows,0);
+                changeInterval[getActiveSubWidget()+index] = $timeout(function () {
+                    updateWidget(widget, index, rows, 0);
                 }, widget.interval);
             });
         }
-       var stopUpdater = function(){
-           $timeout.cancel(changeInterval);
-       }
-       if(widgetChangeEnabled)startUpdater(rows);
-       init(rows);
+        var stopUpdater = function () {
+            _.each(changeInterval,function(interval){
+                $timeout.cancel(interval);
+            });
+            changeInterval = {};
 
-
+        }
+        if (widgetChangeEnabled)startUpdater(rows);
+        init(rows);
 
 
         //controller.$inject = [];
