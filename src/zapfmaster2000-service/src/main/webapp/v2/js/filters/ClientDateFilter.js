e76625d16859dc22e7ce8ed614af5ba3d700fb90
Ben Lenser <lenser.ben@gmail.com>
2014-04-26 21:33:23 +0200

    #189 Implemented widgets

--- src/zapfmaster2000-service/src/main/webapp/v2/js/filters/ClientDateFilter.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/filters/ClientDateFilter.js
@@ -3,10 +3,16 @@ define(['Console','moment'], function (Console,moment) {
     Console.group("Entering ClientDate  filter");
 
     var service = ['ZMConstants', function (c) {
-        return function(input) {
+        return function(input,expression) {
 
+            if(expression=="full"){
+                console.log("yeah")
+                return moment(input, c.SERVER_TIME_FORMAT).format(c.FULL_CLIENT_TIME_FORMAT);
 
-            return moment(input, c.SERVER_TIME_FORMAT).format(c.CLIENT_TIME_FORMAT);
+            }else{
+                return moment(input, c.SERVER_TIME_FORMAT).format(c.CLIENT_TIME_FORMAT);
+
+            }
             // Console.debug("Filter ",input," with Output ", output);
 
         };
