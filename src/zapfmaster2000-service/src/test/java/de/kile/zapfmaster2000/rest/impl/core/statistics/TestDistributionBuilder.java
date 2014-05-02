238fcab3bba62d63f52577cc73d842f3b841e11d
thomaskipar <thomas.kipar@gmail.com>
2014-05-02 16:24:02 +0200

    #190: Added actual curve values to distribution response.

--- src/zapfmaster2000-service/src/test/java/de/kile/zapfmaster2000/rest/impl/core/statistics/TestDistributionBuilder.java
+++ src/zapfmaster2000-service/src/test/java/de/kile/zapfmaster2000/rest/impl/core/statistics/TestDistributionBuilder.java
@@ -27,28 +27,48 @@ public class TestDistributionBuilder {
 		DistributionResponse result = distributionResponseBuilder
 				.retrieveAmountResponse(account);
 
-		assertEquals(6, result.getNormalCurveValues().size());
+		assertEquals(7, result.getNormalCurve().size());
 		assertCurveValueEquals(1.0, 0.017727393647752034, result
-				.getNormalCurveValues().get(0));
+				.getNormalCurve().get(0));
 		assertCurveValueEquals(1.25, 0.21596386605275225, result
-				.getNormalCurveValues().get(1));
-		assertCurveValueEquals(1.5, 0.9678828980765735, result
-				.getNormalCurveValues().get(2));
+				.getNormalCurve().get(1));
+		assertCurveValueEquals(1.5, 0.9678828980765735, result.getNormalCurve()
+				.get(2));
 		assertCurveValueEquals(1.75, 1.5957691216057308, result
-				.getNormalCurveValues().get(3));
-		assertCurveValueEquals(2.0, 0.9678828980765735, result
-				.getNormalCurveValues().get(4));
+				.getNormalCurve().get(3));
+		assertCurveValueEquals(2.0, 0.9678828980765735, result.getNormalCurve()
+				.get(4));
 		assertCurveValueEquals(2.25, 0.21596386605275225, result
-				.getNormalCurveValues().get(5));
+				.getNormalCurve().get(5));
 		assertCurveValueEquals(2.5, 0.017727393647752034, result
-				.getNormalCurveValues().get(6));
+				.getNormalCurve().get(6));
+	}
+
+	@Test
+	public void testNormalAcutalValues() {
+		RankingsBuilder rankingsBuilder = createRankingsBuilder();
+		DistributionResponseBuilder distributionResponseBuilder = new DistributionResponseBuilder(
+				rankingsBuilder);
+		Account account = null;
+
+		DistributionResponse result = distributionResponseBuilder
+				.retrieveAmountResponse(account);
+
+		assertEquals(7, result.getActualCurve().size());
+		assertCurveValueEquals(1.0, 0, result.getActualCurve().get(0));
+		assertCurveValueEquals(1.25, 0, result.getActualCurve().get(1));
+		assertCurveValueEquals(1.5, 0.5, result.getActualCurve().get(2));
+		assertCurveValueEquals(1.75, 0, result.getActualCurve().get(3));
+		assertCurveValueEquals(2.0, 0.5, result.getActualCurve().get(4));
+		assertCurveValueEquals(2.25, 0, result.getActualCurve().get(5));
+		assertCurveValueEquals(2.5, 0, result.getActualCurve().get(6));
 	}
 
 	private void assertCurveValueEquals(double expectedAmount,
 			double exectedUserCount, CurveValue value) {
 		double epsilon = 0.01;
 		assertEquals(expectedAmount, value.getAmount(), epsilon);
-		assertEquals(exectedUserCount, value.getUserCount(), epsilon);
+		assertEquals(exectedUserCount, value.getProbability(), epsilon);
 	}
 
 	private RankingsBuilder createRankingsBuilder() {
