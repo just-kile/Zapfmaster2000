8e3a7bb2e1414dd3ab6eeca031f1f1e44e1a6764
Ben Lenser <lenser.ben@gmail.com>
2014-05-15 22:24:00 +0200

    Added uglify

--- src/zapfmaster2000-service/buildutils/gulpfile.js
+++ src/zapfmaster2000-service/buildutils/gulpfile.js
@@ -1,34 +1,45 @@
+var require = require || {};
 var gulp = require('gulp'),
     bower = require('gulp-bower'),
     rjs = require("gulp-requirejs"),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify');
 
 var basedir = "../src/main/webapp/v2";
 var distdir = "../src/main/webapp/v2/dist";
 
 gulp.task('bower', function () {
+    "use strict";
     return bower()
-        .pipe(gulp.dest(basedir + '/js/vendor'))
+        .pipe(gulp.dest(basedir + '/js/vendor'));
 });
 
 gulp.task('rjs', ['bower'], function () {
+    "use strict";
     rjs({
         baseUrl: basedir + "/js",
         out: 'bootstrap.js',
         name: "bootstrap",
         mainConfigFile: basedir + "/js/bootstrap.js",
         findNestedDependencies: true
-    }).pipe(gulp.dest(distdir + '/js'));
+    })
+        .pipe(uglify({
+            outSourceMap: true
+        }))
+        .pipe(gulp.dest(distdir + '/js'));
 });
 gulp.task('html', function () {
+    "use strict";
 
-})
+});
 
 gulp.task('lint', function () {
-    return gulp.src(basedir+'/js/**/*.js')
+    "use strict";
+
+    return gulp.src(basedir + '/js/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 
-gulp.task('default', ['bower', 'rjs']);
+gulp.task('default', ['bower', 'rjs', 'lint']);
