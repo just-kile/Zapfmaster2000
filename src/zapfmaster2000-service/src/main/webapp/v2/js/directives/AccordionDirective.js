8b0ccd303e2cac431f8586a5e07bf85e2c9ce4d5
Ben Lenser <lenser.ben@gmail.com>
2014-06-27 10:02:48 +0200

    Added temporary icons

--- src/zapfmaster2000-service/src/main/webapp/v2/js/directives/AccordionDirective.js
+++ src/zapfmaster2000-service/src/main/webapp/v2/js/directives/AccordionDirective.js
@@ -11,12 +11,19 @@ define(['Console', 'd3', "Angular",'jQuery'], function (Console, d3, angular,$) 
                 },
                 templateUrl: 'views/directives/AccordionDirective.html',
                 link:function($scope,ele,attrs){
-
+                    var icon = ele.find(".zmo-header-xs > i");
+                    function toggleIcon(){
+                        if(icon.hasClass("glyphicon-chevron-down")){
+                            icon.removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+                        }else{
+                            icon.removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+                        }
+                    }
                     $scope.toggle=function(){
                         // alert("Toggle");
                         var container = $(ele).find(".zm-accordion-container");
                         container.toggleClass("zm-hidden");
-
+                        toggleIcon();
                     };
                     $scope.$watch("title",function(){
                         $scope.title = attrs.title;
