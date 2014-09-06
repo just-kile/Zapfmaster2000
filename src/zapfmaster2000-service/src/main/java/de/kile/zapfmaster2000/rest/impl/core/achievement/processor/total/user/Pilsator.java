c039a56a5066a1b15e0cb895b3f36edf535abdce
thomaskipar <thomas.kipar@gmail.com>
2014-09-06 11:48:37 +0200

    added status achievements

---
+++ src/zapfmaster2000-service/src/main/java/de/kile/zapfmaster2000/rest/impl/core/achievement/processor/total/user/Pilsator.java
@@ -0,0 +1,10 @@
+package de.kile.zapfmaster2000.rest.impl.core.achievement.processor.total.user;
+
+public class Pilsator extends AbstractUserTotalAchievementProcessor{
+
+	@Override
+	protected double getTotal() {
+		return 20;
+	}
+
+}
